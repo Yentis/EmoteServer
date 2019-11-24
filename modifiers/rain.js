6a26b36198b7a0fc432a59aef0b93b62d10e87ea
MaxOhn <ohn.m@hotmail.de>
2019-11-24 01:53:16 +0100

    Fixed drop color & speed now depends on delay instead of height

--- modifiers/rain.js
+++ modifiers/rain.js
@@ -10,7 +10,8 @@ exports.createRainingGIF = (options) => {
             gifhelper.setEncoderProperties(encoder);
 
             let frames = inputGif.frames;
-            let rainGenerator = rainImageGenerator(inputGif.width, inputGif.height, options.value === 1);
+            let glitter = options.value === 1;
+            let rainGenerator = rainImageGenerator(inputGif.width, inputGif.height, glitter, frames[0].delayCentisecs);
 
             for (let i = 0; i < frames.length; i++) {
                 encoder.setDelay(frames[i].delayCentisecs * 10);
@@ -39,7 +40,8 @@ exports.createRainingPNG = (options) => {
             gifhelper.setEncoderProperties(encoder, delay * 10);
 
             let interval = 12;
-            let rainGenerator = rainImageGenerator(width, height, options.value === 1);
+            let glitter = options.value === 1;
+            let rainGenerator = rainImageGenerator(width, height, glitter, delay);
 
             for (let i = 0; i < interval; i++) {
                 let img = new Jimp(image.bitmap);
@@ -52,11 +54,17 @@ exports.createRainingPNG = (options) => {
     });
 };
 
-function rainImageGenerator(width, height, glitter) {
+function rainImageGenerator(width, height, glitter, delay) {
     // Generate single drops
     let drops = [];
     for (let i = 0, amount = (width + height) / 5; i < amount; i++) {
-        drops.push(new Drop(width, height));
+        drops.push(new Drop(width, height, delay));
+    }
+    // Set colors of drops
+    if (glitter) {
+        drops.forEach(drop => drop.setColor(Math.random() * 256, Math.random() * 256, Math.random() * 256));
+    } else {
+        drops.forEach(drop => drop.setColor(0, 120, 255));
     }
 
     const rainGenerator = {
@@ -65,20 +73,12 @@ function rainImageGenerator(width, height, glitter) {
             // Draw raindrops
             for (let i = 0; i < drops.length; i++) {
                 let drop = drops[i];
-                let r, g, b;
-                if (glitter) {
-                    r = Math.random() * 256;
-                    g = Math.random() * 256;
-                    b = Math.random() * 256;
-                } else {
-                    r = 0, g = 120, b = 255;
-                }
                 for (let j = 0; j < drop.len; j++) {
                     for (let k = 0; k < drop.size; k++) {
                         let pos = (Math.floor(drop.y + j) * width + Math.floor(drop.x + k)) * 4;
-                        img.bitmap.data[pos] = r;
-                        img.bitmap.data[pos + 1] = g;
-                        img.bitmap.data[pos + 2] = b;
+                        img.bitmap.data[pos + 0] = drop.r;
+                        img.bitmap.data[pos + 1] = drop.g;
+                        img.bitmap.data[pos + 2] = drop.b;
                         img.bitmap.data[pos + 3] = 255;
                     }
                 }
@@ -91,23 +91,38 @@ function rainImageGenerator(width, height, glitter) {
     return rainGenerator;
 }
 
-function Drop(width, height) {
+function Drop(width, height, delay) {
     this.x = Math.random() * width;
     this.y = Math.random() * height;
 
-    // Speed depends on height (make dependant on frame delay?)
-    let speed = Math.random() * (2 * height / 15) + height / 15;
-    // Map len between 1 and 5, depending on the speed
-    this.len = Math.floor(((speed - (height / 15)) / (height / 5)) * 5 + 1);
-    // Map thickness between 1 and 2, depending on the speed
-    this.size = Math.floor(((speed - (height / 15)) / (height / 5)) * 2 + 1);
+    let { speed, len, size } = resetDrop(delay);
+    this.len = len;
+    this.size = size;
 
     this.fall = function() {
         this.y += speed;
-        // Reset drop
         if (this.y > height) {
             this.y = 0;
-            speed = Math.random() * (2 * height / 15) + height / 15;
+            let { speed, len, size } = resetDrop(delay);
+            this.len = len;
+            this.size = size;
         }
     }
+
+    this.setColor = function(r, g, b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
+
+function resetDrop(delay) {
+    let speed = Math.random();
+    // Map len between 1 and 5, depending on the speed
+    let len = Math.floor(speed * 5 + 1);
+    // Map thickness between 1 and 2, depending on the speed
+    let size = Math.floor(speed * 2 + 1);
+    // Adjust speed to frame delay i.e. the longer the delay, the faster the drop
+    speed = Math.floor(speed * delay + delay);
+    return { speed, len, size };
 }
